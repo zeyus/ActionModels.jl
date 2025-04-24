e7ef4506c26016375394b22ea6e0a205096b1035
Peter Thestrup Waade <peter@waade.net>
2025-04-24 18:17:36 +0200

    made a default PVL delta

--- docs/julia_files/tutorials/example_igt.jl
+++ docs/julia_files/tutorials/example_igt.jl
@@ -94,59 +94,8 @@ n_iterations = 10
 samples = sample(model, sampler, n_iterations)
 
 
-
-
-
-
-
 ### PVL-DELTA ###
-function pvl_delta(agent::Agent, input::Tuple{Int64,Float64})
-
-    deck, reward = input
-
-    learning_rate = agent.parameters[:learning_rate]
-    reward_sensitivity = agent.parameters[:reward_sensitivity]
-    loss_aversion = agent.parameters[:loss_aversion]
-    inv_temperature = agent.parameters[:inv_temperature]
-
-    expected_value = agent.states[:expected_value]
-
-    #Get action probabilities by softmaxing expected values for each deck
-    action_probabilities = softmax(expected_value * inv_temperature)
-
-    #Avoid underflow and overflow
-    action_probabilities = clamp.(action_probabilities, 0.001, 0.999)
-    action_probabilities = action_probabilities / sum(action_probabilities)
-
-    #Calculate prediction error
-    if reward >= 0
-        prediction_error = (reward^reward_sensitivity) - expected_value[deck]
-    else
-        prediction_error =
-            -loss_aversion * (abs(reward)^reward_sensitivity) - expected_value[deck]
-    end
-
-    #Update expected values
-    new_expected_value = [
-        expected_value[deck_idx] + learning_rate * prediction_error * (deck == deck_idx) for deck_idx = 1:4
-    ]
-
-    update_states!(agent, :expected_value, new_expected_value)
-
-    return Categorical(action_probabilities)
-end
-
-action_model = ActionModel(
-    pvl_delta,
-    parameters = (
-        learning_rate = Parameter(0.1),
-        reward_sensitivity = Parameter(0.5),
-        inv_temperature = Parameter(1),
-        loss_aversion = Parameter(1),
-    ),
-    states = (;expected_value = State(zeros(Float64, 4))), #TODO: Fix this
-)
-
+action_model = ActionModel(PVLDelta(n_decks = 4))
 
 model = create_model(
     action_model,
--- src/defining_models/premade_models/pvl_delta.jl
+++ src/defining_models/premade_models/pvl_delta.jl
@@ -1,46 +1,77 @@
-# function pvl_delta(agent::Agent, input::Tuple{Int64,Float64})
+export PVLDelta
 
-#     deck, reward = input
+Base.@kwdef struct PVLDelta <: AbstractPremadeModel
+    n_decks::Int64 = 4
+    learning_rate::Float64 = 0.1
+    action_precision::Float64 = 1
+    reward_sensitivity::Float64 = 0.5
+    loss_aversion::Float64 = 1
+    initial_value::Array{Float64} = zeros(Float64, n_decks)
+end
 
-#     learning_rate = agent.parameters["learning_rate"]
-#     reward_sensitivity = agent.parameters["reward_sensitivity"]
-#     loss_aversion = agent.parameters["loss_aversion"]
-#     inv_temperature = agent.parameters["inv_temperature"]
+function ActionModel(config::PVLDelta)
 
-#     expected_value = agent.states["expected_value"]
+    function pvl_delta(agent::Agent, input::Tuple{Int64,Float64})
 
-#     #Get action probabilities by softmaxing expected values for each deck
-#     action_probabilities = softmax(expected_value * inv_temperature)
+        deck, reward = input
 
-#     #Avoid underflow and overflow
-#     action_probabilities = clamp.(action_probabilities, 0.001, 0.999)
-#     action_probabilities = action_probabilities / sum(action_probabilities)
+        learning_rate = agent.parameters[:learning_rate]
+        reward_sensitivity = agent.parameters[:reward_sensitivity]
+        loss_aversion = agent.parameters[:loss_aversion]
+        action_precision = agent.parameters[:action_precision]
 
-#     #Calculate prediction error
-#     if reward >= 0
-#         prediction_error = (reward^reward_sensitivity) - expected_value[deck]
-#     else
-#         prediction_error =
-#             -loss_aversion * (abs(reward)^reward_sensitivity) - expected_value[deck]
-#     end
+        expected_value = agent.states[:expected_value]
 
-#     #Update expected values
-#     new_expected_value = [
-#         expected_value[deck_idx] + learning_rate * prediction_error * (deck == deck_idx) for deck_idx = 1:4
-#     ]
+        #Get action probabilities by softmaxing expected values for each deck
+        action_probabilities = softmax(expected_value * action_precision)
 
-#     update_states!(agent, "expected_value", new_expected_value)
+        #Avoid underflow and overflow
+        action_probabilities = clamp.(action_probabilities, 0.001, 0.999)
+        action_probabilities = action_probabilities / sum(action_probabilities)
 
-#     return Categorical(action_probabilities)
-# end
+        #Calculate prediction error
+        if reward >= 0
+            prediction_error = (reward^reward_sensitivity) - expected_value[deck]
+        else
+            prediction_error =
+                -loss_aversion * (abs(reward)^reward_sensitivity) - expected_value[deck]
+        end
 
-# # agent = init_agent(
-# #     pvl_delta,
-# #     parameters = Dict(
-# #         "learning_rate" => 0.1,
-# #         "reward_sensitivity" => 0.5,
-# #         "inv_temperature" => 1,
-# #         "loss_aversion" => 1,
-# #     ),
-# #     states = Dict("expected_value" => zeros(Float64, 4)),
-# # )
+        #Update expected values
+        new_expected_value = [
+            expected_value[deck_idx] +
+            learning_rate * prediction_error * (deck == deck_idx) for deck_idx = 1:n_decks
+        ]
+
+        update_states!(agent, :expected_value, new_expected_value)
+
+        return Categorical(action_probabilities)
+    end
+
+    parameters = (
+            learning_rate = Parameter(config.learning_rate),
+            reward_sensitivity = Parameter(config.reward_sensitivity),
+            action_precision = Parameter(config.action_precision),
+            loss_aversion = Parameter(config.loss_aversion),
+            initial_value = InitialStateParameter(config.initial_value, :expected_value),
+        )
+
+    states = (; expected_value = State(zeros(Float64, 4)))
+
+    observations = (;
+        deck = Observation(Int64),
+        reward = Observation(Float64),
+    )
+
+    actions = (;
+        choice = Action(Categorical),
+    )
+
+    return ActionModel(
+        pvl_delta,
+        parameters = parameters,
+        states = states,
+        observations = observations,
+        actions = actions,
+    )
+end
