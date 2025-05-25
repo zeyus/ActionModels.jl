83645236eef252f7a8142092a457609a7317f0ad
Peter Thestrup Waade <peter@waade.net>
2025-05-25 15:55:48 +0200

    minor renaming

--- docs/julia_files/B_user_guide/6_workflow_tools.jl
+++ docs/julia_files/B_user_guide/6_workflow_tools.jl
@@ -1,4 +1,4 @@
-# # Addtional tools for Bayesian analysis workflows in Julia
+# # Addtional tools for Bayesian analysis workflows
 # Although individual workdflows may vary depending on the requirements of the given project, there exists a range of standard methods that are recommended to be part of cognitive modelling workflows to ensure that analysis is completed correctly.
 # These are often introduced in more detail by various sources: see Hess et al., 2025, Wilson & Collins, 2019 or Lee & Wagenmakers, 2014 for examples.
 # This includes prior and posterior checks, which ensure that the model produces appropriate behavioural, precision analysis and parameter recovery, which ensure that parameters can be accurately estimated from the data, and model comparison, which allows to comin terms of how well they describe the data.
