c95b27a84f37b520886d1205371057981af48406
Peter Thestrup Waade <peter@waade.net>
2025-05-26 16:41:41 +0200

    jget example finished

--- docs/julia_files/D_tutorials_examples/example_jget.jl
+++ docs/julia_files/D_tutorials_examples/example_jget.jl
@@ -58,10 +58,11 @@ disallowmissing!(JGET_data, [:pdi_total])
 show(JGET_data)
 
 
-# For this example, we will subset the data to only include six subjects in total, across the three epxerimental sessions.
+# For this example, we will subset the data to only include four subjects in total, across the three epxerimental sessions.
 # This makes the runtime much shorter. Simply skip this step if you want to use the full dataset.
 
-JGET_data = filter(row -> row[:ID] in [20, 30, 40, 50, 60, 74], JGET_data)
+JGET_data = filter(row -> row[:ID] in [20, 40, 60, 70], JGET_data)
+
 
 
 # ## Creating the model
@@ -81,12 +82,33 @@ observation_cols = :outcome
 session_cols = [:ID, :session]
 
 # We then create the fulle model. We use a hierarchical regression model to predict the parameters of the Rescorla-Wagner model based on the session number and the PDI score.
+# First we will set appropriate priors for the regression coefficients.
+# For the action noise, the outcome of the regression will be exponentiated before it is used in the model, so pre-transformed outcomes around 3 (exp(3) ≈ 20) are among the most extreme values to be expected.
+# For the learning rate, the outcome of the regression will be passed through a logistic function, so valuespre-transformed outcomes around around 5 (logistic(5) ≈ 0.993) are among the most extreme values to be expected.
+# This means that we should limit priors to be fairly narrow, so that the linear regression does not go too far into inappropriate parameter space, which will increase the runtime of the fitting process.
+# 
+
+plot(Normal(0,0.3), label = "Intercept")
+plot!(Normal(0,0.5), label = "Effect of PDI")
+plot!(truncated(Normal(0,0.3), lower = 0), label = "Random intercept std")
+title!("Regression priors for the Rescorla-Wagner model")
+xlabel!("Regression coefficient")
+ylabel!("Density")
+
+
+regression_prior = RegressionPrior(
+            β = [Normal(0,0.3), Normal(0,0.5)],
+            σ = truncated(Normal(0, 0.3),lower = 0),
+        )
+
+population_model = [
+        Regression(@formula(learning_rate ~ 1 + pdi_total + (1 | ID)), logistic, regression_prior),
+        Regression(@formula(action_noise ~ 1 +  pdi_total + (1 | ID)), exp, regression_prior),
+    ]
+
 model = create_model(
     action_model,
-    [
-        Regression(@formula(learning_rate ~ 1 + session + pdi_total + (1 | ID)), logistic),
-        Regression(@formula(action_noise ~ 1 + session + pdi_total + (1 | ID)), exp),
-    ],
+    population_model,
     JGET_data,
     action_cols = action_cols,
     observation_cols = observation_cols,
@@ -105,8 +127,10 @@ import Enzyme: set_runtime_activity, Reverse
 ad_type = AutoEnzyme(; mode = set_runtime_activity(Reverse, true));
 
 ## Fit model ##
-chns = sample_posterior!(model, n_chains = 1, n_samples = 300, ad_type = ad_type)
+chns = sample_posterior!(model, n_chains = 1, n_samples = 500, ad_type = ad_type)
 
+
+plot(chns)
 # We can now inspect the results of the fitting process.
 
 #TODO: look at the beta values of the chains
@@ -134,51 +158,64 @@ show(states_df)
 
 
 
+# ## Comparing to a simple random model
+# We can also compare the Rescorla-Wagner model to a simple random model, which simply samples actions from a Gaussian distribution with a fixed mean $\mu$ and standard deviation $\beta$.
 
+# ### Creating the Gaussian random action model 
+# In this model, actions are sampled from a Gaussian distribution with a fixed mean and standard deviation.
+# This meanst that there are two parameters in the action model: the action noise $\beta$ and the action mean $\mu$.
 
+function gaussian_random(agent::Agent, input::T) where {T<:Real}
 
+    β = agent.parameters[:action_noise]
+    μ = agent.parameters[:mean]
 
+    return Normal(μ, β)
+end
 
+action_model = ActionModel(
+    gaussian_random,
+    parameters = (action_noise = Parameter(1), mean = Parameter(50)),
+)
 
+# ### Fitting the model
+# We also set priors hfor this simpler model.
+# Here we set the priors separately for the mean and the noise, since they are on very different scales.
+# We center the priors for the mean at 50, as that is the iddle of the range of actions.
+# The priors for the noise are similar to those used with the Rescorla-Wagner model.
 
+mean_regression_prior = RegressionPrior(
+            β = [Normal(50,10), Normal(0,10)],
+            σ = truncated(Normal(0, 10), lower = 0),
+        )
+noise_regression_prior = RegressionPrior(
+            β = [Normal(0,0.3), Normal(0,0.5)],
+            σ = truncated(Normal(0, 0.3), lower = 0),
+        )
 
+population_model = [
+        Regression(@formula(mean ~ 1 + pdi_total + (1 | ID)), mean_regression_prior),
+        Regression(@formula(action_noise ~ 1 + pdi_total + (1 | ID)), exp, noise_regression_prior),
+    ]
 
+simple_model = create_model(
+    action_model,
+    population_model,
+    JGET_data,
+    action_cols = action_cols,
+    observation_cols = observation_cols,
+    session_cols = session_cols,
+)
 
-# ### GAUSSIAN RANDOM ###
-# function gaussian_choice(agent::Agent, input::T) where {T<:Real}
+## Set AD backend ##
+using ADTypes: AutoEnzyme
+import Enzyme: set_runtime_activity, Reverse 
+ad_type = AutoEnzyme(; mode = set_runtime_activity(Reverse, true));
 
-#     action_noise = agent.parameters[:action_noise]
-#     mean = agent.parameters[:mean]
+## Fit model ##
+chns = sample_posterior!(simple_model, n_chains = 1, n_samples = 500, ad_type = ad_type)
 
-#     return Normal(mean, action_noise)
-# end
+#TODO: plot the results
 
-# action_model = ActionModel(
-#     gaussian_choice,
-#     parameters = (action_noise = Parameter(1), mean = Parameter(50)),
-# )
-
-# model = create_model(
-#     action_model,
-#     [
-#         Regression(@formula(mean ~ 1 + session + pdi_total + (1 | ID))),
-#         Regression(@formula(action_noise ~ 1 + session + pdi_total + (1 | ID)), exp),
-#     ],
-#     JGET_data,
-#     action_cols = action_cols,
-#     observation_cols = observation_cols,
-#     session_cols = session_cols,
-# )
-
-# # AD = AutoForwardDiff()
-# # AD = AutoReverseDiff(; compile = false)
-# AD = AutoReverseDiff(; compile = true)
-# # import Mooncake;
-# # AD = AutoMooncake(; config = nothing);
-
-# #Set samplings settings
-# sampler = NUTS(-1, 0.65; adtype = AD)
-# n_iterations = 10
-
-# samples = sample(model, sampler, n_iterations)
+#TODO: model comparison
 
