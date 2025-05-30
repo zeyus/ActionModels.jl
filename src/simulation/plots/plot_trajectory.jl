c3295725741f5d5f9858fbd067cd31365c1943ab
Peter Thestrup Waade <peter@waade.net>
2025-05-30 21:31:13 +0200

    WIP: only lacking doscstrings for model fitting now!

--- src/simulation/plots/plot_trajectory.jl
+++ src/simulation/plots/plot_trajectory.jl
@@ -1,3 +1,24 @@
+"""
+    f(agent::Agent, target_state::Symbol, index::Union{Nothing,Vector{Int}}=nothing)
+
+Plot the trajectory (history) of a state variable for an agent over time.
+
+This plotting recipe visualizes the time course of a given state variable from an agent's history. If the state is multivariate (e.g., a vector or array), you can specify an index to plot a particular dimension. If the state is univariate, the index should be left as `nothing` (the default).
+
+# Arguments
+- `agent::Agent`: The agent whose state trajectory will be plotted.
+- `target_state::Symbol`: The name of the state variable to plot (e.g., `:expected_value`).
+- `index::Union{Nothing,Vector{Int}}`: (Optional) Index for multivariate states (e.g., `[1]` for the first element).
+
+# Example
+```jldoctest; setup = :(using ActionModels, StatsPlots; agent = init_agent(ActionModel(RescorlaWagner()), save_history=true); simulate!(agent, [1.0, 0.5, 0.2]))
+julia> plot(agent, :expected_value)
+```
+For a multivariate state (e.g., a vector):
+```jldoctest; setup = :(using ActionModels, StatsPlots; agent = init_agent(ActionModel(RescorlaWagner(type=:categorical, n_categories = 3)), save_history=true); simulate!(agent, [1,2,1]))
+julia> plot(agent, :expected_value, [1])
+```
+"""
 @recipe function f(
     agent::Agent,
     target_state::Symbol,
@@ -19,7 +40,7 @@
 
         #Extract the state history for the given index
         index = CartesianIndex(index...)
-        state_history = map(i -> state_history[index], index)
+        state_history = map(i -> state_history[i][index], 1:length(state_history))
     end
 
     #Replace missings with NaNs for plotting
